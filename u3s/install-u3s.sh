5259a40ca322c5a388791168087e41c082f87d1d
Kirikae <22813189+kirikae@users.noreply.github.com>
2025-09-09 11:14:17 +1000

    Added install of k3s

--- u3s/install-u3s.sh
+++ u3s/install-u3s.sh
@@ -2,3 +2,18 @@
 
 set -ouex pipefail
 
+# install package.json stuffs
+export IMAGE_NAME=u3s
+/ctx/packages.sh
+
+mkdir -p /etc/rancher/k3s
+cat << EOF > /etc/rancher/k3s/kubelet.config
+apiVersion: kubelet.config.k8s.io/v1beta1
+kind: KubeletConfiguration
+shutdownGracePeriod: 30s
+shutdownGracePeriodCriticalPods: 10s
+EOF
+
+# tweak os-release
+sed -i 's/^VARIANT_ID=.*/VARIANT_ID=u3s/' /usr/lib/os-release
+sed -i 's/^VARIANT=.*/VARIANT="uCore K3s"/' /usr/lib/os-release
--- u3s/post-install-u3s.sh
+++ u3s/post-install-u3s.sh
@@ -2,3 +2,10 @@
 
 set -ouex pipefail
 
+if [[ -f /usr/share/selinux/packages/k3s.pp ]]; then
+    semodule --verbose --install /usr/share/selinux/packages/k3s.pp
+fi
+
+firewall-cmd --permanent --add-port=8472/udp # Flannel VXLAN
+firewall-cmd --permanent --add-port=10250/tcp # Kubelet metrics
+firewall-cmd --permanent --add-port=51810-51821/udp # Flannel wireguard IPv4 & IPv6
