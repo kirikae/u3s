91328cb7e1ecdad64f097053aac49ea433c0c4c9
Kirikae <22813189+kirikae@users.noreply.github.com>
2025-09-11 12:03:27 +1000

    Updated actions to match crio and k3s versions

--- u3s/install-u3s.sh
+++ u3s/install-u3s.sh
@@ -6,7 +6,7 @@ set -ouex pipefail
 export IMAGE_NAME=u3s
 /ctx/packages.sh
 
-mv /usr/bin/calico-linux-amd64 /usr/bin/kubectl-calico
+mv /usr/bin/calicoctl-linux-amd64 /usr/bin/kubectl-calico
 
 # tweak os-release
 sed -i 's/^VARIANT_ID=.*/VARIANT_ID=u3s/' /usr/lib/os-release
