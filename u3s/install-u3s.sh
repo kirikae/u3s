d4082b5932cb64bd78bd00d0b2ee7f4827d65378
Kirikae <22813189+kirikae@users.noreply.github.com>
2025-09-12 08:24:45 +1000

    Add Helm binary, split workflow get_bins

--- u3s/install-u3s.sh
+++ u3s/install-u3s.sh
@@ -2,11 +2,16 @@
 
 set -ouex pipefail
 
+ls -lah /ctx/
+
 # install package.json stuffs
 export IMAGE_NAME=u3s
 /ctx/packages.sh
 
 mv /usr/bin/calicoctl-linux-amd64 /usr/bin/kubectl-calico
+tar xvf /ctx/${HELM_BINARY_TARBALL} linux-amd64/helm
+mv linux-amd64/helm /usr/bin/
+chmod 0755 /usr/bin/helm
 
 # tweak os-release
 sed -i 's/^VARIANT_ID=.*/VARIANT_ID=u3s/' /usr/lib/os-release
